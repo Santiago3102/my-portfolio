060835ceccc767546673471fd0dfd9940de0260b
Santiago3102 <santiago.gomez3@utp.edu.co>
2024-07-29 05:56:14 -0500

    Faltan detalles de diseño

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,27 +1,45 @@
 import React from 'react';
-import { FaEnvelope } from 'react-icons/fa';
+import { FaEnvelope, FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
 
 const Contact = () => {
   return (
     <section id="contact" className="py-20">
       <div className="container mx-auto text-center">
         <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
-        <p className="mb-6">
-          You can reach me via email at{' '}
-          <a 
-            href="mailto:santiago.gomez3@utp.edu.co"
-            className="text-emerald-500 hover:underline"
-          >
-            santiago.gomez3@utp.edu.co
-          </a>
-        </p>
+        
         <a 
           href="mailto:santiago.gomez3@utp.edu.co"
-          className="inline-flex items-center px-6 py-3 bg-emerald-600 text-white font-semibold rounded-lg hover:bg-emerald-700 transition-colors duration-300"
+          className="inline-flex items-center px-6 py-3 bg-emerald-600 text-white font-semibold rounded-lg hover:bg-emerald-700 transition-colors duration-300 mb-6"
         >
           <FaEnvelope className="mr-2" />
           Send an Email
         </a>
+        <div className="flex justify-center gap-6">
+          <a 
+            href="https://github.com/Santiago3102" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-emerald-600 hover:text-emerald-700"
+          >
+            <FaGithub className="text-3xl" />
+          </a>
+          <a 
+            href="https://linkedin.com/in/santiago-gomez-077319267/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-emerald-600 hover:text-emerald-700"
+          >
+            <FaLinkedin className="text-3xl" />
+          </a>
+          <a 
+            href="https://www.instagram.com/santiago_gomez.31/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-emerald-600 hover:text-emerald-700"
+          >
+            <FaInstagram className="text-3xl" />
+          </a>
+        </div>
       </div>
     </section>
   );
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useState, useEffect } from 'react';
-import { FaJs, FaReact, FaNodeJs, FaPython, FaJava, FaHtml5, FaCss3, FaFileCode } from 'react-icons/fa'; // Asegúrate de tener todos los íconos necesarios
+import { FaJs, FaReact, FaNodeJs, FaPython, FaJava, FaHtml5, FaCss3, FaFileCode } from 'react-icons/fa';
 
 interface Repo {
   id: number;
   name: string;
+  full_name: string;
   description?: string;
   html_url: string;
   language?: string;
@@ -14,7 +15,7 @@ interface Repo {
 
 const languageIcons: { [key: string]: JSX.Element } = {
   JavaScript: <FaJs />,
-  TypeScript: <FaJs />, 
+  TypeScript: <FaJs />,
   Python: <FaPython />,
   Java: <FaJava />,
   HTML: <FaHtml5 />,
@@ -31,13 +32,20 @@ const Projects = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const orgRepos = await fetchRepos('https://api.github.com/orgs/YOUR_ORG_NAME/repos');
-        const personalRepos = await fetchRepos('https://api.github.com/users/Santiago3102/repos');
+        const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN; // Usar el token de las variables de entorno
+        const orgRepos1 = await fetchRepos('https://api.github.com/orgs/FullStack-Elite-Bootcamp-IV/repos', token);
+        const orgRepos2 = await fetchRepos('https://api.github.com/orgs/Proyecto-Cinema/repos', token);
+        const personalRepos = await fetchRepos('https://api.github.com/users/Santiago3102/repos', token);
 
-        if (Array.isArray(orgRepos) && Array.isArray(personalRepos)) {
-          setProjects([...orgRepos, ...personalRepos]);
+        if (Array.isArray(orgRepos1) && Array.isArray(orgRepos2) && Array.isArray(personalRepos)) {
+          const allRepos = [...orgRepos1, ...orgRepos2, ...personalRepos];
+          const reposWithImages = await Promise.all(allRepos.map(async (repo) => {
+            const imageUrl = await fetchRepoImage(repo, token);
+            return { ...repo, homepage: imageUrl };
+          }));
+          setProjects(reposWithImages);
         } else {
-          console.error('Error: One or both of the fetched data are not arrays.');
+          console.error('Error: One or more of the fetched data are not arrays.');
         }
       } catch (error) {
         console.error('Error fetching repos:', error);
@@ -49,9 +57,16 @@ const Projects = () => {
     fetchData();
   }, []);
 
-  const fetchRepos = async (url: string): Promise<Repo[]> => {
+  const fetchRepos = async (url: string, token?: string): Promise<Repo[]> => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(url, {
+        headers: {
+          'Authorization': token ? `token ${token}` : ''
+        }
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
       const data = await response.json();
       return Array.isArray(data) ? data : [];
     } catch (error) {
@@ -60,34 +75,64 @@ const Projects = () => {
     }
   };
 
+  const fetchRepoImage = async (repo: Repo, token?: string): Promise<string | undefined> => {
+    try {
+      const response = await fetch(`https://api.github.com/repos/${repo.full_name}/readme`, {
+        headers: { 
+          'Authorization': token ? `token ${token}` : '',
+          'Accept': 'application/vnd.github.v3.raw' 
+        }
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const readmeContent = await response.text();
+      const imageUrlMatch = readmeContent.match(/!\[.*?\]\((.*?)\)/);
+      return imageUrlMatch ? imageUrlMatch[1] : undefined;
+    } catch (error) {
+      console.error('Error fetching repo image:', error);
+      return undefined;
+    }
+  };
+
   if (loading) return <p className="text-center py-4 text-gray-300">Loading...</p>;
 
   return (
-    <section id="projects" className="py-40 text-emerald-200">
+    <section id="projects" className="sm:pt-52 text-emerald-200">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold mb-12">My Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project) => (
-            <a 
-              key={project.id} 
-              href={project.html_url} 
-              target="_blank" 
-              rel="noopener noreferrer" 
-              className="block p-6 bg-zinc-800 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+            <a
+              key={project.id}
+              href={project.html_url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block bg-zinc-800 rounded-lg border border-emerald-600 shadow-md hover:shadow-xl hover:border-emerald-300 transition-all duration-300 flex flex-col"
             >
-              {project.homepage && (
-                <img
-                  src={project.homepage}
-                  alt={project.name}
-                  className="w-full h-40 object-cover rounded-t-lg"
-                />
-              )}
-              <div className="relative mt-4">
-                <h3 className="text-2xl font-semibold mb-2">{project.name}</h3>
-                <p className="text-white mb-4">{project.description || 'No description available'}</p>
-                <div className="flex gap-2 mb-4">
-                  {project.language && languageIcons[project.language]}
-                </div>
+              <div className="relative flex-shrink-0 overflow-hidden rounded-t-lg">
+                {project.homepage ? (
+                  <img
+                    src={project.homepage}
+                    alt={project.name}
+                    className="w-full h-48 object-cover transform scale-105 transition-transform duration-300 hover:scale-110"
+                  />
+                ) : (
+                  <div className="w-full h-48 bg-zinc-700 rounded-t-lg"></div>
+                )}
+              </div>
+              <div className="p-4 flex flex-col flex-grow">
+                <h3 className="text-2xl font-semibold mb-2 text-emerald-300">{project.name}</h3>
+                <p className="text-white flex-grow overflow-y-auto mb-4">
+                  {project.description || 'No description available'}
+                </p>
+              </div>
+              <div className="p-4 flex justify-center items-center bg-zinc-700 rounded-b-lg">
+                {project.language && languageIcons[project.language] ? (
+                  languageIcons[project.language]
+                ) : (
+                  <FaFileCode className="text-gray-500" /> 
+                )}
               </div>
             </a>
           ))}
